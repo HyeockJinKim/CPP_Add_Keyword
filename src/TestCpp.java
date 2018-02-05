5db1931039cb353b150115c80cc689b420b8d2c5
KimHyeockJin <gurwls9628@naver.com>
2018-02-06 03:24:05 +0900

    cpp antlr 설정

---
+++ src/TestCpp.java
@@ -0,0 +1,18 @@
+import gen.CPP14Lexer;
+import gen.CPP14Parser;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class TestCpp {
+    public static void main(String[] args) throws IOException {
+        String filePath = Paths.get(".","src", "test").toString();
+        CPP14Lexer lexer = new CPP14Lexer(new ANTLRFileStream(filePath));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CPP14Parser parser = new CPP14Parser(tokens);
+        ParseTree tree = parser.translationunit();
+    }
+}
